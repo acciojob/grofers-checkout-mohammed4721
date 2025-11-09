dfcd4d1e1c9db43d714b7fa1b3825ce215d93403
Mohammed Khaja Mohinuddin <161879901+mohammed4721@users.noreply.github.com>
2025-11-09 23:05:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-//your JS code here. If required.
-let level = document.getElementById("level");
+let table = document.getElementsByTagName("table")[0];
+let prices = document.querySelectorAll(".price");
+	
+const getSumBtn = document.createElement("button");
+getSumBtn.style.cursor = "pointer";
+getSumBtn.append("Get Total Price");
+document.body.appendChild(getSumBtn);
 
-let count = 0;
-let currentLevel = level;
+getSumBtn.addEventListener("click", ()=>{
+	const oldRow = document.getElementById("totalRow");
+	if(oldRow) {
+		oldRow.remove();
+	}
+	const tableRow = document.createElement("tr");
+	tableRow.id = "totalRow"
+	const tableDataPrices = document.createElement("td");
+	const tableDataPricesSum = document.createElement("td");
+	tableDataPrices.textContent = "Total Price";
+	tableDataPricesSum.textContent = getSum();
+	tableDataPricesSum.innerHTML = `<span id="ans">${getSum()}</span>`;
+	tableRow.appendChild(tableDataPrices);
+	tableRow.appendChild(tableDataPricesSum);
+	table.appendChild(tableRow);
+})
 
-while (currentLevel != null) {
-	currentLevel = currentLevel.parentNode;
-	count++
+function getSum() {
+	let sum = 0;
+	for(let price of prices) {
+		sum += parseInt(price.textContent)
+	}
+	return sum;
 }
-
-alert("The level of the element is: " + count);
